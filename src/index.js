3e6f846ee7d85cb634d61c3433ef02ffdecb7a2e
yomiojo001 <yomiojo001@gmail.com>
2019-11-01 07:19:51 +0100

    second-react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Person = ({img, name, job, children}) =>{
+const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+        return(
+            <article className="person">
+                <img src={url} alt="person" />
+                <h4>{name}</h4>
+                <h4>{job}</h4>
+                {children}
+            </article>
+        );
+};
+
+const PersonList = () =>{
+    return <section className="person-list">
+                <Person img="34" name="john" job="developer">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </Person>
+                <Person img="27" name="bob" job="designer">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </Person>
+                <Person img="20" name="dave" job="engineer">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </Person>
+                <Person img="52" name="pepe" job="painter">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </Person>
+                <Person img="48" name="matt" job="doctor">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </Person>
+                <Person img="30" name="paul" job="dentist">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </Person>
+            </section>
+};
+
+
+
+
+ReactDOM.render(<PersonList/> , document.getElementById("root"));
